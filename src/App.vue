c2da79627abb19d3d794a7d5401d7b88532d39e9
HarukiKinoshita <harukikinoshita@outlook.com>
2022-02-04 23:14:58 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <!-- <LinkedView :time-distance-object="TsukubaExpress" /> -->
+    <LinkedView :display-list="displayList" />
+    <div v-for="(item, index) in dataList" :key="index">
+      <input type="checkbox" :id="item.line" :value="item" v-model="displayList">
+      <label :for="item.line">{{ item.displayName }}
+        <span :style="`width: 200px; background: ${item.color} `">.</span>
+      </label>
+    </div>
+    <!-- {{ dataList }}
+    <br>
+    {{ displayList }} -->
+  </div>
+</template>
+
+<script>
+import LinkedView from './components/LinkedView.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LinkedView
+  },
+  data: () => {
+    return {
+      displayList: [],
+      dataList: [
+        {
+          line: "TsukubaExpress", 
+          displayName: "つくばエクスプレス快速",
+          color: "#ff0000"
+        },
+        {
+          line: "Joban",
+          displayName: "常磐線特別快速",
+          color: "#0067c0"
+        },
+        {
+          line: "Chuo",
+          displayName: "中央特快",
+          color: "#F15A22"
+        },
+        {
+          line: "KyotoShinkaisoku",
+          displayName: "京都線新快速",
+          color: "#0072BC"
+        }
+      ]
+    }
+  },
+  mounted() {
+    this.displayList = this.dataList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/LinkedView.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 300" style="border: 1px solid black;">
+      <!-- 軸 -->
+      <line x1="0" y1="50" x2="1200" y2="50" stroke="lightgray" stroke-width="0.5"/>
+      <line x1="0" y1="150" x2="1200" y2="150" stroke="lightgray" stroke-width="0.5" />
+
+      <!-- 駅ごとのデータ -->
+      <g v-for="(item, index) in timeDistanceData.filter(el => displayList.map(el => el.line).includes(el.line) && el.time !== '')" :key="index">
+        <!-- Time line -->
+        <circle
+          :cx="item.time*15 + 30"
+          cy="50"
+          r="2"
+          fill="black"
+        />
+        <!-- Distance line -->
+        <circle
+          :cx="item.distance*15 + 30"
+          cy="150"
+          r="2"
+          fill="black"
+        />
+        <!-- Path -->
+        <line
+          :x1="item.time*15 + 30"
+          y1="50"
+          :x2="item.distance*15 + 30"
+          y2="150"
+          :stroke="findColor(item.line)"
+          stroke-width="1"
+        />
+        <!-- <polygon :points="`30,50 `+(item.time*15 + 30)+`,50 `+(item.distance*15 + 30)+`, 150 30, 150`" fill="red" opacity="0.05" stroke="black" stroke-width="1" /> -->
+        <!-- 所要時間 -->
+        <text
+          :x="item.time*15 + 28"
+          y="40"
+          font-size="10"
+        >
+          {{ item.time }}
+        </text>
+        <!-- 営業キロ -->
+        <text
+          :x="item.distance*15 + 28"
+          y="160"
+          font-size="10"
+          writing-mode="tb"
+        >
+          {{ item.distance }}km
+        </text>
+        <!-- 駅名 -->
+        <text
+          :x="item.distance*15 + 28"
+          y="200"
+          font-size="10"
+          :fill="findColor(item.line)"
+          writing-mode="tb"
+        >
+          {{ item.station }}
+        </text>
+      </g>
+    </svg>
+    <!-- <div v-for="(item, index) in TsukubaExpress" :key="index">
+      {{ item.station }} {{ item.time }}, {{ item.time !== '' }}
+    </div> -->
+  </div>
+</template>
+
+<script>
+import TsukubaExpress from '../data/tsukuba_express.json'
+import Joban from '../data/joban.json'
+import Chuo from '../data/chuo.json'
+import KyotoShinkaisoku from '../data/kyoto_shinkaisoku.json'
+
+export default {
+  data: () => {
+    return {
+      TsukubaExpress: TsukubaExpress,
+      timeDistanceData: TsukubaExpress.concat(Joban).concat(Chuo).concat(KyotoShinkaisoku)
+    }
+  },
+  props: {
+    // timeDistanceObject: Array 
+    displayList: Array
+  },
+  methods: {
+    findColor(lineName) {
+      return this.displayList.find(el => el.line == lineName)['color']
+    }
+  }
+}
+</script>
