d9dab673639a8e15bb0750d426e193d7e8b62455
HarukiKinoshita <harukikinoshita@outlook.com>
2022-02-17 17:30:14 +0900

    Add Google Analytics

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueCookie from 'vue-cookie';
+import VueGtag from "vue-gtag";
 
 Vue.use(VueCookie)
+Vue.use(VueGtag, {
+  config: { id: process.env.GA_ID }
+})
 Vue.config.productionTip = false
 
 new Vue({
